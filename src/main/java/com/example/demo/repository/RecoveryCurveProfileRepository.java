a777d30dba73a1f9fc6182421111c8c96e5947de
bala-amypo <bala@amypo.in>
2025-12-27 04:07:11 +0000

    User commit

--- src/main/java/com/example/demo/repository/RecoveryCurveProfileRepository.java
+++ src/main/java/com/example/demo/repository/RecoveryCurveProfileRepository.java
@@ -1,12 +1,10 @@
-package com.example.recovery.repository;
+package com.example.demo.repository;
 
-import com.example.recovery.entity.RecoveryCurveProfile;
+import com.example.demo.model.RecoveryCurveProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface RecoveryCurveProfileRepository
-        extends JpaRepository<RecoveryCurveProfile, Long> {
-
+public interface RecoveryCurveProfileRepository extends JpaRepository<RecoveryCurveProfile, Long> {
     List<RecoveryCurveProfile> findBySurgeryTypeOrderByDayNumberAsc(String surgeryType);
 }
