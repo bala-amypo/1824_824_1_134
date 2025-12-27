ddd9091a8340b24b79e215f173396e082a8b392f
bala-amypo <bala@amypo.in>
2025-12-27 04:00:42 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,13 +1,16 @@
-package com.example.healthcare.controller;
+package com.example.demo.controller;
 
-import com.example.healthcare.dto.AuthRequest;
-import com.example.healthcare.dto.RegisterRequest;
-import com.example.healthcare.entity.AppUser;
-import com.example.healthcare.service.AuthService;
+import com.example.demo.dto.AuthRequest;
+import com.example.demo.dto.AuthResponse;
+import com.example.demo.dto.RegisterRequest;
+import com.example.demo.service.AuthService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/auth")
+@Tag(name = "Auth")
 public class AuthController {
 
     private final AuthService authService;
@@ -17,12 +20,12 @@ public class AuthController {
     }
 
     @PostMapping("/register")
-    public AppUser register(@RequestBody RegisterRequest request) {
-        return authService.register(request);
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authService.register(request));
     }
 
     @PostMapping("/login")
-    public AppUser login(@RequestBody AuthRequest request) {
-        return authService.login(request);
+    public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
+        return ResponseEntity.ok(authService.login(request));
     }
 }
