cfd98efcd44611df45467bf476ecf1a8031e19a7
bala-amypo <bala@amypo.in>
2025-12-27 04:15:51 +0000

    User commit

--- src/main/java/com/example/demo/service/DailySymptomLogService.java
+++ src/main/java/com/example/demo/service/DailySymptomLogService.java
@@ -1,10 +1,11 @@
+package com.example.demo.service;
+
+import com.example.demo.model.DailySymptomLog;
+
 import java.util.List;
 
 public interface DailySymptomLogService {
-
-    DailySymptomLog recordLog(DailySymptomLog log);
-
+    DailySymptomLog recordSymptomLog(DailySymptomLog log);
+    DailySymptomLog updateSymptomLog(Long id, DailySymptomLog updated);
     List<DailySymptomLog> getLogsByPatient(Long patientId);
-
-    DailySymptomLog getLogById(Long id);
 }
