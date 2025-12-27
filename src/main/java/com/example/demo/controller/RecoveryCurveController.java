03371359e351ab158d3cfdf4ce73a36e16bcb1be
bala-amypo <bala@amypo.in>
2025-12-27 04:02:45 +0000

    User commit

--- src/main/java/com/example/demo/controller/RecoveryCurveController.java
+++ src/main/java/com/example/demo/controller/RecoveryCurveController.java
@@ -1,37 +1,36 @@
-package com.example.recovery.controller;
+package com.example.demo.controller;
 
-import com.example.recovery.entity.RecoveryCurveProfile;
-import com.example.recovery.service.RecoveryCurveService;
-import org.springframework.http.HttpStatus;
+import com.example.demo.model.RecoveryCurveProfile;
+import com.example.demo.service.RecoveryCurveService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/recovery-curves")
+@Tag(name = "Recovery Curves")
 public class RecoveryCurveController {
 
-    private final RecoveryCurveService service;
+    private final RecoveryCurveService recoveryCurveService;
 
-    public RecoveryCurveController(RecoveryCurveService service) {
-        this.service = service;
+    public RecoveryCurveController(RecoveryCurveService recoveryCurveService) {
+        this.recoveryCurveService = recoveryCurveService;
     }
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public RecoveryCurveProfile createCurveEntry(
-            @RequestBody RecoveryCurveProfile profile) {
-        return service.createCurveEntry(profile);
+    public ResponseEntity<RecoveryCurveProfile> create(@RequestBody RecoveryCurveProfile curve) {
+        return ResponseEntity.ok(recoveryCurveService.createCurveEntry(curve));
     }
 
     @GetMapping("/surgery/{surgeryType}")
-    public List<RecoveryCurveProfile> getCurveForSurgery(
-            @PathVariable String surgeryType) {
-        return service.getCurveForSurgery(surgeryType);
+    public ResponseEntity<List<RecoveryCurveProfile>> getCurveForSurgery(@PathVariable String surgeryType) {
+        return ResponseEntity.ok(recoveryCurveService.getCurveForSurgery(surgeryType));
     }
 
     @GetMapping
-    public List<RecoveryCurveProfile> getAllCurves() {
-        return service.getAllCurves();
+    public ResponseEntity<List<RecoveryCurveProfile>> getAll() {
+        return ResponseEntity.ok(recoveryCurveService.getAllCurves());
     }
 }
