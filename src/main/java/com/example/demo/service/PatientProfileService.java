b67d45e65aea0a4031691d2d5c43fc73f89ffc41
bala-amypo <bala@amypo.in>
2025-12-27 04:16:25 +0000

    User commit

--- src/main/java/com/example/demo/service/PatientProfileService.java
+++ src/main/java/com/example/demo/service/PatientProfileService.java
@@ -1,16 +1,14 @@
 package com.example.demo.service;
 
-import com.example.demo.entity.PatientProfile;
+import com.example.demo.model.PatientProfile;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PatientProfileService {
-
-    PatientProfile createPatient(PatientProfile patient);
-
+    PatientProfile createPatient(PatientProfile profile);
     PatientProfile getPatientById(Long id);
-
     List<PatientProfile> getAllPatients();
-
     PatientProfile updatePatientStatus(Long id, boolean active);
+    Optional<PatientProfile> findByPatientId(String patientId);
 }
